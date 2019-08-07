90239a33185c8509208e67e08efa14d186c5ea8e
Tian Jiang <riersy2324@gmail.com>
2019-08-07 17:19:33 +0800

    Update script-d3.js

--- d3charts/script-d3.js
+++ d3charts/script-d3.js
@@ -840,229 +840,224 @@ function kernelEpanechnikov(k) {
 
 /* Bubble */
 var svg8 = d3
-.select("#my_datavis")
-.append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .select("#my_datavis")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("/assets/data/d3charts/d3-8.csv", function(data) {
-  var x = d3
-  .scaleLinear()
-  .domain([0, 45000])
-  .range([0, width]);
-  svg8
-    .append("g")
-    .attr("transform", "translate(0, " + height + ")")
-    .call(d3.axisBottom(x).ticks(5));
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv", function(data) {
+  var x = d3.scaleLinear()
+            .domain([0, 45000])
+            .range([0, width])
+  var xAxis = svg8.append("g")
+      .attr("transform", "translate(0, " + height + ")")
+      .call(d3.axisBottom(x).ticks(5))
 
-  svg8
-    .append("text")
-    .attr("text-anchor", "end")
-    .attr("x", width - 90)
-    .attr("y", height + 30)
-    .attr("font-size", 10)
-    .text("GDP per capita");
-
-  var y = d3
-  .scaleLinear()
-  .domain([40, 80])
-  .range([height, 0]);
-  svg8.append("g").call(d3.axisLeft(y));
-
+  svg8.append("text")
+     .attr("text-anchor", "end")
+     .attr("x", width - 90)
+     .attr("y", height + 45)
+     .attr("font-size", 12)
+     .text("GDP per capita")
+  
+  var y = d3.scaleLinear()
+            .domain([ 40, 80 ])
+            .range([ height, 0 ])
+  var yAxis = svg8.append("g")
+      .call(d3.axisLeft(y))
+  
   var highlight = function(d) {
-    d3.selectAll(".bubbles").style("opacity", 0.1);
-    d3.selectAll("." + d).style("opacity", 1);
-  };
-
+    d3.selectAll(".bubbles")
+      .style("opacity", 0.1)
+    d3.selectAll("."+d)
+      .style("opacity", 1)
+  }
+  
   var nohighlight = function(d) {
-    d3.selectAll(".bubbles").style("opacity", 1);
-  };
+    d3.selectAll(".bubbles")
+      .style("opacity", 1)
+  }
+  
+  svg8.append("text")
+     .attr("text-anchor", "end")
+     .attr("x", 0)
+     .attr("y", -20)
+     .text("Life Expectancy")
+     .attr("font-size", 12)  
+     .attr("text-anchor", "start")
+  
+  var z = d3.scaleSqrt()
+            .domain([ 200000, 1310000000 ])
+            .range([ 2, 30 ])
+  
+  var colorArr = ["#F17878", "#B1ECBD", "#FFC66A", "#458FCD", "#5DC4A8"]
+  var groups = ["Asia", "Europe", "Americas", "Africa", "Oceania"]
+  var color = d3.scaleOrdinal()
+                  .domain(groups)
+                  .range(colorArr);
+  svg8.append("rect")
+      .attr("x", 210)
+      .attr("y", 80)
+      .attr("width", 100)
+      .attr("height", 165)
+      .style("fill", "none")
+      .style("stroke", "#081200")
+      .style("stroke-width", 0.5)
+  var clip = svg8.append("defs").append("svg:clipPath")
+            .attr("id", "clip")
+            .append("svg:rect")
+            .attr("width", width )
+            .attr("height", height )
+            .attr("x", 0)
+            .attr("y", 0);
+   var countries = d3.select("body")
+       .append("div")
+       .style("position", "absolute")
+       .style("z-index", "10")
+       .style("visibility", "hidden")
+       .style("background-color", "#000000")
+       .style("color", "#FFFFFF")
+       .style("font-size", 8)
+       .style("padding", "5px")
+       .style("border", "solid")
+       .style("border-radius", "50px");
 
-  svg8
-    .append("text")
-    .attr("text-anchor", "end")
-    .attr("x", 0)
-    .attr("y", -20)
-    .text("Life Expectancy")
-    .attr("font-size", 10)
-    .attr("text-anchor", "start");
-
-  var z = d3
-  .scaleSqrt()
-  .domain([200000, 1310000000])
-  .range([2, 30]);
-
-  var colorArr = ["#F17878", "#B1ECBD", "#FFC66A", "#458FCD", "#5DC4A8"];
-  var groups = ["Asia", "Europe", "Americas", "Africa", "Oceania"];
-  var color = d3
-  .scaleOrdinal()
-  .domain(groups)
-  .range(colorArr);
-  svg8
-    .append("rect")
-    .attr("x", 210)
-    .attr("y", 80)
-    .attr("width", 100)
-    .attr("height", 165)
-    .style("fill", "none")
-    .style("stroke", "#081200")
-    .style("stroke-width", 0.5);
-
-  var countries = d3
-  .select("body")
-  .append("div")
-  .style("position", "absolute")
-  .style("z-index", "10")
-  .style("visibility", "hidden")
-  .style("background-color", "#000000")
-  .style("color", "#FFFFFF")
-  .style("font-size", 8)
-  .style("padding", "5px")
-  .style("border", "solid")
-  .style("border-radius", "50px");
-
-  var mouseover = function(d) {
-    countries.style("visibility", "visible");
-    countries
-      .html(d.country)
-      .style("left", event.pageX + 5 + "px")
-      .style("top", event.pageY + 5 + "px");
-  };
-  var mousemove = function(d, i) {
-    countries
-      .html(d.country)
-      .style("left", event.pageX + 5 + "px")
-      .style("top", event.pageY + 5 + "px");
-  };
+  var brush = d3.brush()
+                .extent( [ [0,0], [width,height] ] )
+                .on("end", updateChart)
+  var scatter = svg8.append("g")
+                     .attr("clip-path", "url(#clip)")
+  
+  var mouseover = function(d) { 
+    countries.style("visibility", "visible")
+    countries.html(d.country)
+             .style("left", (event.pageX+5)+"px")
+             .style("top", (event.pageY+5) + "px")    
+  }
+  var mousemove = function(d,i) {
+    countries.html(d.country)
+             .style("left", (event.pageX+5)+"px")
+             .style("top", (event.pageY+5) + "px")
+  }
   var mouseleave = function(d) {
-    countries.style("visibility", "hidden");
-  };
+     countries.style("visibility", "hidden")
+  }  
+  
+  scatter.append("g")
+     .selectAll("dot")
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("class", function(d) { return "bubbles " + d.continent})
+     .attr("cx", function(d) { return x(d.gdpPercap)})
+     .attr("cy", function(d) { return y(d.lifeExp)})
+     .attr("r", function(d) { return z(d.pop)})
+     .style("fill", function(d) { return color(d.continent)})
+     .style("stroke", "#ffffff")
+     .style("opacity", 0.95)
+     .on("mouseover", mouseover)
+     .on("mouseout", mousemove)
+     .on("mouseleave", mouseleave)
+     
+   scatter.append("g")
+     .attr("class", "brush")
+     .call(brush)
 
-  svg8
-    .append("g")
-    .selectAll("dot")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("class", function(d) {
-    return "bubbles " + d.continent;
-  })
-    .attr("cx", function(d) {
-    return x(d.gdpPercap);
-  })
-    .attr("cy", function(d) {
-    return y(d.lifeExp);
-  })
-    .attr("r", function(d) {
-    return z(d.pop);
-  })
-    .style("fill", function(d) {
-    return color(d.continent);
-  })
-    .style("stroke", "#ffffff")
-    .style("opacity", 0.95)
-    .on("mouseover", mouseover)
-    .on("mouseout", mousemove)
-    .on("mouseleave", mouseleave);
-  var legends = [10000000, 100000000, 1000000000];
-  var xLegend = 250;
-  svg8
-    .selectAll("legend")
-    .data(legends)
-    .enter()
-    .append("circle")
-    .attr("cx", xLegend)
-    .attr("cy", function(d) {
-    return height - 40 - z(d);
-  })
-    .attr("r", function(d) {
-    return z(d);
-  })
-    .style("fill", "none")
-    .style("stroke", "#000000")
-    .style("stroke-width", 0.5);
+  var legends = [10000000, 100000000, 1000000000]
+  var xLegend = 250
+  svg8.selectAll("legend")
+      .data(legends)
+      .enter()
+      .append("circle")
+      .attr("cx", xLegend)
+      .attr("cy", function(d) { return height - 40 - z(d)})
+      .attr("r", function(d) { return z(d)})
+      .style("fill", "none")
+      .style("stroke", "#000000")
+      .style("stroke-width", 0.5)
+  
+  svg8.selectAll("legend")
+      .data(legends)
+      .enter()
+      .append("line")
+      .attr("x1", function(d) { return xLegend + z(d) })
+      .attr("y1", function(d) { return height - 40 - z(d)})
+      .attr("x2", 285)
+      .attr("y2", function(d) { return height - 40 - z(d)})
+      .attr("stroke", "#000000")
+      .style("stroke-dasharray", ("2, 2"))      
+      .style("stroke-width", 0.5)
+  
+  svg8.selectAll("legend")
+      .data(legends)
+      .enter()
+      .append("text")
+      .attr("x", 285)
+      .attr("y", function(d) { return height - 40 - z(d)})
+      .text( function(d) { return d/1000000})
+      .style("font-size", 8)
+      .attr("alignment-baseline", "middle")
 
-  svg8
-    .selectAll("legend")
-    .data(legends)
-    .enter()
-    .append("line")
-    .attr("x1", function(d) {
-    return xLegend + z(d);
-  })
-    .attr("y1", function(d) {
-    return height - 40 - z(d);
-  })
-    .attr("x2", 285)
-    .attr("y2", function(d) {
-    return height - 40 - z(d);
-  })
-    .attr("stroke", "#000000")
-    .style("stroke-dasharray", "2, 2")
-    .style("stroke-width", 0.5);
+  svg8.append("text")
+      .attr("x", xLegend)
+      .attr("y", height - 20)
+      .text("Population (M)")
+      .style("font-size", 9.5) 
+      .attr("text-anchor", "middle")
+  
+  var size = 20
+  svg8.selectAll("rects")
+      .data(groups)
+      .enter()
+      .append("circle")
+      .attr("cx", 222)
+      .attr("cy", function(d, i) { return 90 + i * 15})
+      .attr("r", 6)
+      .style("fill", function(d) { return color(d)})
+      .on("mouseover", highlight)
+      .on("mouseleave", nohighlight)
+  
+  svg8.selectAll("labels")
+      .data(groups)
+      .enter()
+      .append("text")
+      .text(function(d) { return d })
+      .style("font-size", 9)
+      .attr("x", 235)
+      .attr("y", function(d, i) { return 90 + i * 15})
+      .attr("alignment-baseline", "middle")
+      .style("fill", function(d) { return color(d)})
+      .on("mouseover", highlight)
+      .on("mouseleave", nohighlight)
+  
+  var idleTimeout
+  
+  function idled() { idleTimeout = null; }
+  
+  function updateChart() {
+    extent = d3.event.selection
+    
+    if(!extent){
+          if (!idleTimeout) return idleTimeout = setTimeout(idled, 1000);
+          x.domain([0,45000])
+          y.domain([40,80])
+        }else{
+          x.domain([extent[0][0], extent[1][0]].map(x.invert, x));
+          y.domain([extent[1][1], extent[0][1]].map(y.invert, y));
 
-  svg8
-    .selectAll("legend")
-    .data(legends)
-    .enter()
-    .append("text")
-    .attr("x", 285)
-    .attr("y", function(d) {
-    return height - 40 - z(d);
-  })
-    .text(function(d) {
-    return d / 1000000;
-  })
-    .style("font-size", 8)
-    .attr("alignment-baseline", "middle");
-
-  svg8
-    .append("text")
-    .attr("x", xLegend)
-    .attr("y", height - 20)
-    .text("Population (M)")
-    .style("font-size", 9.5)
-    .attr("text-anchor", "middle");
-
-  var size = 20;
-  svg8
-    .selectAll("rects")
-    .data(groups)
-    .enter()
-    .append("circle")
-    .attr("cx", 222)
-    .attr("cy", function(d, i) {
-    return 90 + i * 15;
-  })
-    .attr("r", 6)
-    .style("fill", function(d) {
-    return color(d);
-  })
-    .on("mouseover", highlight)
-    .on("mouseleave", nohighlight);
-
-  svg8
-    .selectAll("labels")
-    .data(groups)
-    .enter()
-    .append("text")
-    .text(function(d) {
-    return d;
-  })
-    .style("font-size", 9)
-    .attr("x", 235)
-    .attr("y", function(d, i) {
-    return 90 + i * 15;
-  })
-    .attr("alignment-baseline", "middle")
-    .style("fill", function(d) {
-    return color(d);
-  })
-    .on("mouseover", highlight)
-    .on("mouseleave", nohighlight);
-});
+          scatter.select(".brush").call(brush.move, null)
+        }
+        yAxis.transition().duration(1000).call(d3.axisLeft(y))
+        xAxis.transition().duration(1000).call(d3.axisBottom(x).ticks(5))
+        scatter
+          .selectAll("circle")
+          .transition().duration(1000)
+          .attr("cx", function (d) { return x(d.gdpPercap); } )
+          .attr("cy", function (d) { return y(d.lifeExp); } )
+  }    
+})
 /* Bubble */
 
 /* Drag */
